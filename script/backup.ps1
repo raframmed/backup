abee24124b7bf05d80a2a2fd50053e900d11f2c3
raframmed <33767092+raframmed@users.noreply.github.com>
2018-02-22 21:25:01 +0100

    Explicación del codigo

--- script/backup.ps1
+++ script/backup.ps1
@@ -1,3 +1,8 @@
+<# 
+$a será el nombre del usuario de la carpeta cuya queremos hacer la copia de seguridad.
+$b será la carpeta de destino donde se guardará el archivo comprimido con la copia de seguridad
+#>
+
 $a = $args[0]
 $b = $args[1]
 
