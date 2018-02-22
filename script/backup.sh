79f1ae67ffe788aff177403ec5d915c8b8e6305a
raframmed <33767092+raframmed@users.noreply.github.com>
2018-02-22 21:26:23 +0100

    explicacion del codigo

--- script/backup.sh
+++ script/backup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+ 
+#$1 será el nombre del usuario de la carpeta cuya queremos hacer la copia de seguridad.
+#$2 será la carpeta de destino donde se guardará el archivo comprimido con la copia de seguridad
 
 tar -zcvf backup.tar.gz /home/$1/
 
 mv backup.tar.gz $2 
 
-#$1 = nombre de usuario
-#$2 = ruta de destino
+
